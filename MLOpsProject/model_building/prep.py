2bb6d6c530e7adb6ba2237e54537200c7bfcbd02
dsivaranjani <46396619+dsivaranjani@users.noreply.github.com>
2026-04-05 18:29:53 +0530

    Update prep.py

--- MLOpsProject/model_building/prep.py
+++ MLOpsProject/model_building/prep.py
@@ -9,7 +9,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 # for hugging face space authentication to upload files
 from huggingface_hub import login, HfApi
-
+print("Tourism-prep.py")
 # Define constants for the dataset and output paths
 api = HfApi(token=os.getenv("HF_TOKEN"))
 DATASET_PATH = "hf://datasets/RanjaniD/Tourism-Package-Prediction/Tourism.csv"
